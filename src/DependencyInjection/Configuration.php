a0310c971d6ab9329bd43bd797541ff57715b23a
Joachim Løvgaard <joachim@loevgaard.dk>
2018-05-29 15:50:33 +0200

    initial commit

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Loevgaard\SyliusBrandPlugin\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('loevgaard_sylius_brand');
+
+        return $treeBuilder;
+    }
+}
---
+++ src/LoevgaardSyliusBrandPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Loevgaard\SyliusBrandPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class LoevgaardSyliusBrandPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
