ed697927f0bf85ee1b4468959fe525a53c037a39
Joachim Løvgaard <joachim@loevgaard.dk>
2018-06-26 16:01:40 +0200

    added test, but no finished

---
+++ tests/Form/Type/BrandChoiceTypeTest.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace Loevgaard\SyliusBrandPlugin\Form\Type;
+
+use Loevgaard\SyliusBrandPlugin\Entity\Brand;
+use Loevgaard\SyliusBrandPlugin\Repository\BrandRepositoryInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Component\Form\PreloadedExtension;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class BrandChoiceTypeTest extends TypeTestCase
+{
+    /**
+     * @var MockObject|BrandRepositoryInterface
+     */
+    private $brandRepository;
+
+    protected function setUp()
+    {
+        $brand = new Brand();
+        $brand
+            ->setId(1)
+            ->setName('brand name')
+            ->setSlug('brand-name')
+        ;
+
+        $this->brandRepository = $this->createMock(BrandRepositoryInterface::class);
+        $this->brandRepository->method('findBy')->willReturn([$brand]);
+
+        parent::setUp();
+    }
+
+    protected function getExtensions()
+    {
+        // create a type instance with the mocked dependencies
+        $type = new BrandChoiceType($this->brandRepository);
+
+        return array(
+            // register the type instances with the PreloadedExtension
+            new PreloadedExtension(array($type), array()),
+        );
+    }
+
+    public function testSubmitValidData()
+    {
+        $formData = array(
+            'test' => 'test',
+            'test2' => 'test2',
+        );
+
+        //$objectToCompare = new TestObject();
+        // $objectToCompare will retrieve data from the form submission; pass it as the second argument
+        $form = $this->factory->create(BrandChoiceType::class);
+
+        //$object = new TestObject();
+        // ...populate $object properties with the data stored in $formData
+
+        // submit the data to the form directly
+        //$form->submit($formData);
+
+        //$this->assertTrue($form->isSynchronized());
+
+        // check that $objectToCompare was modified as expected when the form was submitted
+        //$this->assertEquals($object, $objectToCompare);
+
+        $view = $form->createView();
+        //$children = $view->children;
+
+        //foreach (array_keys($formData) as $key) {
+            //$this->assertArrayHasKey($key, $children);
+        //}
+    }
+}
