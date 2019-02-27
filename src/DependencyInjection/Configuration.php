397e054dfe43926bba34190013a970414a32857a
Igor Mukhin <igor.mukhin@gmail.com>
2019-02-27 19:23:44 +0200

    Added: Resource configuration to plugin for future upgrade

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 namespace Loevgaard\SyliusBrandPlugin\DependencyInjection;
 
+use Loevgaard\SyliusBrandPlugin\Doctrine\ORM\BrandImageRepository;
+use Loevgaard\SyliusBrandPlugin\Doctrine\ORM\BrandRepository;
+use Loevgaard\SyliusBrandPlugin\Entity\Brand;
+use Loevgaard\SyliusBrandPlugin\Entity\BrandImage;
+use Loevgaard\SyliusBrandPlugin\Entity\BrandImageInterface;
+use Loevgaard\SyliusBrandPlugin\Entity\BrandInterface;
+use Loevgaard\SyliusBrandPlugin\Form\Type\BrandImageType;
+use Loevgaard\SyliusBrandPlugin\Form\Type\BrandType;
+use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
+use Sylius\Component\Resource\Factory\Factory;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -17,6 +29,65 @@ final class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('loevgaard_sylius_brand');
 
+        $rootNode
+            ->addDefaultsIfNotSet()
+            ->children()
+                ->scalarNode('driver')->defaultValue(SyliusResourceBundle::DRIVER_DOCTRINE_ORM)->end()
+            ->end()
+        ;
+
+        $this->addResourcesSection($rootNode);
+
         return $treeBuilder;
+    }
+
+    /**
+     * @param ArrayNodeDefinition $node
+     */
+    private function addResourcesSection(ArrayNodeDefinition $node): void
+    {
+        $node
+            ->children()
+                ->arrayNode('resources')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->arrayNode('brand')
+                            ->addDefaultsIfNotSet()
+                            ->children()
+                                ->variableNode('options')->end()
+                                ->arrayNode('classes')
+                                    ->addDefaultsIfNotSet()
+                                    ->children()
+                                        ->scalarNode('model')->defaultValue(Brand::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('interface')->defaultValue(BrandInterface::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('controller')->defaultValue(ResourceController::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('repository')->defaultValue(BrandRepository::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('factory')->defaultValue(Factory::class)->end()
+                                        ->scalarNode('form')->defaultValue(BrandType::class)->cannotBeEmpty()->end()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('brand_image')
+                            ->addDefaultsIfNotSet()
+                            ->children()
+                                ->variableNode('options')->end()
+                                ->arrayNode('classes')
+                                    ->addDefaultsIfNotSet()
+                                    ->children()
+                                        ->scalarNode('model')->defaultValue(BrandImage::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('interface')->defaultValue(BrandImageInterface::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('controller')->defaultValue(ResourceController::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('repository')->defaultValue(BrandImageRepository::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('factory')->defaultValue(Factory::class)->end()
+                                        ->scalarNode('form')->defaultValue(BrandImageType::class)->cannotBeEmpty()->end()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
     }
 }
--- src/LoevgaardSyliusBrandPlugin.php
+++ src/LoevgaardSyliusBrandPlugin.php
@@ -5,9 +5,20 @@ declare(strict_types=1);
 namespace Loevgaard\SyliusBrandPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class LoevgaardSyliusBrandPlugin extends Bundle
 {
     use SyliusPluginTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSupportedDrivers(): array
+    {
+        return [
+            SyliusResourceBundle::DRIVER_DOCTRINE_ORM,
+        ];
+    }
 }
