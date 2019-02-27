f6dfe2996f29d413278aa0f85a1786bf513a5c7f
Igor Mukhin <igor.mukhin@gmail.com>
2019-02-27 17:44:13 +0200

    Added: Few first tests for Brand

---
+++ tests/Controller/BrandApiTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Loevgaard\SyliusBrandPlugin\Controller;
+
+use Loevgaard\SyliusBrandPlugin\Entity\BrandInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+final class BrandApiTest extends AbstractApiTestCase
+{
+    /**
+     * @test
+     */
+    public function it_does_not_allow_to_show_brands_list_when_access_is_denied()
+    {
+        $this->loadDefaultFixtureFiles([
+            'resources/brands.yml',
+        ]);
+
+        $this->client->request('GET', $this->getBrandUrl());
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'authentication/access_denied_response', Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_allow_to_show_brand_when_it_does_not_exist()
+    {
+        $this->loadDefaultFixtureFiles([
+            'authentication/api_administrator.yml',
+            'resources/brands.yml',
+        ]);
+
+        $this->client->request('GET', $this->getBrandUrl(-1), [], [], static::$authorizedHeaderWithAccept);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'error/not_found_response', Response::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * @test
+     */
+    public function it_allows_indexing_brands()
+    {
+        $this->loadDefaultFixtureFiles([
+            'authentication/api_administrator.yml',
+            'resources/brands.yml',
+        ]);
+
+        $this->client->request('GET', $this->getBrandUrl(), [], [], static::$authorizedHeaderWithAccept);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'brand/index_response', Response::HTTP_OK);
+    }
+
+    /**
+     * @param BrandInterface|string $brand
+     * @return string
+     */
+    private function getBrandUrl($brand = '')
+    {
+        return sprintf('/api/v1/brands/%s',
+            $brand instanceof BrandInterface ? $brand->getSlug() : $brand
+        );
+    }
+}
