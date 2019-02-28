0bf8ce32cbc285c4f3c147a44449e164e880172b
Igor Mukhin <igor.mukhin@gmail.com>
2019-02-28 19:00:57 +0200

    Added: Missed changes at ProductTypeExtension

--- src/Form/Extension/ProductTypeExtension.php
+++ src/Form/Extension/ProductTypeExtension.php
@@ -4,24 +4,32 @@ declare(strict_types=1);
 
 namespace Loevgaard\SyliusBrandPlugin\Form\Extension;
 
-use Loevgaard\SyliusBrandPlugin\Form\Type\BrandChoiceType;
+use Loevgaard\SyliusBrandPlugin\Form\Type\BrandAutocompleteChoiceType;
 use Sylius\Bundle\ProductBundle\Form\Type\ProductType;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ProductTypeExtension extends AbstractTypeExtension
 {
+    /**
+     * {@inheritdoc}
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('brand', BrandChoiceType::class, [
+        $builder->add('brand', BrandAutocompleteChoiceType::class, [
             'placeholder' => 'loevgaard_sylius_brand.form.product.select_brand',
             'label' => 'loevgaard_sylius_brand.form.product.brand',
             'required' => false,
         ]);
     }
 
-    public function getExtendedType(): string
+    /**
+     * {@inheritdoc}
+     */
+    public static function getExtendedTypes()
     {
-        return ProductType::class;
+        return [
+            ProductType::class,
+        ];
     }
 }
