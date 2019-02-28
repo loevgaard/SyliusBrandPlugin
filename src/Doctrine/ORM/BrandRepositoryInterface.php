868fe8a333d09509e37bdb809c599009f5cd2969
Igor Mukhin <igor.mukhin@gmail.com>
2019-02-28 11:23:43 +0200

    Improved: BrandRepository

--- src/Doctrine/ORM/BrandRepositoryInterface.php
+++ src/Doctrine/ORM/BrandRepositoryInterface.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace Loevgaard\SyliusBrandPlugin\Doctrine\ORM;
 
+use Doctrine\ORM\QueryBuilder;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 
 interface BrandRepositoryInterface extends RepositoryInterface
 {
+    /**
+     * @return QueryBuilder
+     */
+    public function createListQueryBuilder(): QueryBuilder;
 }
