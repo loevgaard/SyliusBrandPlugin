e1c402995999092a42c7e39fd6100131c3491718
Igor Mukhin <igor.mukhin@gmail.com>
2019-03-19 15:02:16 +0200

    Removed: Method createListQueryBuilder from repository

--- src/Doctrine/ORM/BrandRepository.php
+++ src/Doctrine/ORM/BrandRepository.php
@@ -4,19 +4,10 @@ declare(strict_types=1);
 
 namespace Loevgaard\SyliusBrandPlugin\Doctrine\ORM;
 
-use Doctrine\ORM\QueryBuilder;
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 
 class BrandRepository extends EntityRepository implements BrandRepositoryInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function createListQueryBuilder(): QueryBuilder
-    {
-        return $this->createQueryBuilder('o');
-    }
-
     /**
      * {@inheritdoc}
      */
--- src/Doctrine/ORM/BrandRepositoryInterface.php
+++ src/Doctrine/ORM/BrandRepositoryInterface.php
@@ -4,17 +4,11 @@ declare(strict_types=1);
 
 namespace Loevgaard\SyliusBrandPlugin\Doctrine\ORM;
 
-use Doctrine\ORM\QueryBuilder;
 use Loevgaard\SyliusBrandPlugin\Model\BrandInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 
 interface BrandRepositoryInterface extends RepositoryInterface
 {
-    /**
-     * @return QueryBuilder
-     */
-    public function createListQueryBuilder(): QueryBuilder;
-
     /**
      * @param string $phrase
      *
