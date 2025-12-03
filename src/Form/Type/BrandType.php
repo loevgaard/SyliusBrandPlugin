8f1aa10388674123f789129d14c11660a73fcce8
Joachim Løvgaard <joachim@loevgaard.dk>
2025-12-03 13:50:35 +0100

    Made the images a live collection type

--- src/Form/Type/BrandType.php
+++ src/Form/Type/BrandType.php
@@ -6,9 +6,9 @@ namespace Loevgaard\SyliusBrandPlugin\Form\Type;
 
 use Sylius\Bundle\ResourceBundle\Form\EventSubscriber\AddCodeFormSubscriber;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\UX\LiveComponent\Form\Type\LiveCollectionType;
 
 final class BrandType extends AbstractResourceType
 {
@@ -22,12 +22,13 @@ final class BrandType extends AbstractResourceType
             ->add('name', TextType::class, [
                 'label' => 'loevgaard_sylius_brand.form.brand.name',
             ])
-            ->add('images', CollectionType::class, [
+            ->add('images', LiveCollectionType::class, [
                 'entry_type' => BrandImageType::class,
                 'allow_add' => true,
                 'allow_delete' => true,
                 'by_reference' => false,
                 'label' => 'loevgaard_sylius_brand.form.brand.images',
+                'block_name' => 'entry',
             ]);
     }
 
