1db56a30d3a477f0a5b5a23877858b913d29090c
Joachim Løvgaard <joachim@loevgaard.dk>
2025-12-04 11:56:58 +0100

    Fix deps

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreErrorsOnPackage('fakerphp/faker', [ErrorType::SHADOW_DEPENDENCY])
 ;
